ad6bd283f7edf5822a2fbc4bc310eb116360669a
Benjamin Pantzis <bpantzis01@gmail.com>
2014-04-16 17:42:33 -0400

    Added base weapon class.

---
+++ Source/FM/FMWeapon.h
@@ -0,0 +1,18 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FMWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AFMWeapon : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
