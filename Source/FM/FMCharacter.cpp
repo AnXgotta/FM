64fd7728cb38af48fb5f7578e12943e959e770a4
Benjamin Pantzis <bpantzis01@gmail.com>
2014-04-27 07:36:10 -0400

    Added walking animation and adjusted animation tree to walk at slower speeds.  Fixed stamina usage on sprint.

--- Source/FM/FMCharacter.cpp
+++ Source/FM/FMCharacter.cpp
@@ -911,31 +911,13 @@ bool AFMCharacter::IsRunning() const {
 void AFMCharacter::SetRunning(bool bNewSprint, bool bToggle){
 	
 	if (Role < ROLE_Authority){
-		if (bNewSprint){
-			if (GEngine){
-				GEngine->AddOnScreenDebugMessage(-1, DEBUG_MSG_TIME, FColor::Blue, TEXT("Character: CLIENT : StartSprint "));
-			}
-		}else{
-			if (GEngine){
-				GEngine->AddOnScreenDebugMessage(-1, DEBUG_MSG_TIME, FColor::Blue, TEXT("Character: CLIENT : StopSprint "));
-			}
-		}
-		
 		ServerSetRunning(bNewSprint, bToggle);
-	}else{
-		if (bNewSprint){
-			if (GEngine){
-				GEngine->AddOnScreenDebugMessage(-1, DEBUG_MSG_TIME, FColor::Blue, TEXT("Character: SERVER : StartSprint "));
-			}
-		}
-		else{
-			if (GEngine){
-				GEngine->AddOnScreenDebugMessage(-1, DEBUG_MSG_TIME, FColor::Blue, TEXT("Character: SERVER : StopSprint "));
-			}
-		}
 	}
 	bWantsToRun = bNewSprint;
-	//bWantsToSprintToggled = bNewSprint && bToggle;
+
+	if (!bWantsToRun){
+		ResetStaminaCooldown();
+	}
 
 	// UpdateRunningSounds(bNewSprint);
 }
